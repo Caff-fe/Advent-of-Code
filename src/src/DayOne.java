8466f9a646a3d74b039ba4b4eb1a88540ededbf9
Caff-fe <dimoy73@gmail.com>
2022-12-01 10:57:40 +0200

    Reading the entries from the given input

--- src/src/DayOne.java
+++ src/src/DayOne.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class DayOne {
     public static void main(String[] args) {
 
+        try {
+            File input = new File("D:/Advent of Code/DayOne/inputOne.txt");
+            Scanner reader = new Scanner(input);
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error");
+            e.printStackTrace();
+        }
     }
 }
